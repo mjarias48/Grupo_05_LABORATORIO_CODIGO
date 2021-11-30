0049ced4c3eaeee6238563378862b0d3e47cbcd1
mjarias48 <65546768+mjarias48@users.noreply.github.com>
2021-11-30 16:23:51 -0500

    Grupo_05

---
+++ js/main.js
@@ -0,0 +1,38 @@
+window.sr = ScrollReveal();
+	
+	sr.reveal('.scroll-login1', {
+     duration:3000,
+     origin: 'left',
+     distance: '-100px'
+	});
+
+        sr.reveal('.scroll-home1', {
+     duration:3000,
+     origin: 'bottom',
+     distance: '-100px'
+    });
+
+    sr.reveal('.scroll-about1', {
+     duration:3000,
+     origin: 'right',
+     distance: '-100px'
+    });
+
+	sr.reveal('.scroll-home', {
+     duration:3000,
+     origin: 'top',
+     distance: '-300px'
+	});
+
+	sr.reveal('.scroll-about', {
+     duration:3000,
+     origin: 'left',
+     distance: '-300px'
+	});
+
+	sr.reveal('.scroll-login', {
+     duration:3000,
+     origin: 'left',
+     distance: '-1200px'
+	});
+
